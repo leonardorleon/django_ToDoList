96220c8d1e90351175556f90ba6e59e8ede83ec2
Leonardo Rodriguez <leonardorleon@gmail.com>
2022-05-01 20:25:36 +0200

    Added a form to the list of items, such that they are interactive. Also added logic for:
    -Saving checked/unchecked status on items.
    -Adding a new item to the list.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,6 +7,29 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        
+        # Handle the changes in the checkmarks of the To-Do list
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                
+                item.save()
+
+        # Handle the addition of a new item to a To-Do list
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("item content")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete = False) 
+            else:
+                print("invalid input")
+
     return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
