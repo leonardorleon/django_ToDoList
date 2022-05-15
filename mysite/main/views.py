bcfb1c3e4cde543319c2ddc22f6bd6d85a273137
Leonardo Rodriguez <leonardorleon@gmail.com>
2022-05-15 16:49:31 +0200

    Made some changes so that todo lists are related to users and so that the user can only see their own todo lists. \nNexts steps would probably be to make login/logout interactions easie, adding them as buttons to the sidebar and now that the basic functionality is there, to add finishing touches to make the page look more complete

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -8,29 +8,33 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        
-        # Handle the changes in the checkmarks of the To-Do list
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            print(response.POST)
+            
+            # Handle the changes in the checkmarks of the To-Do list
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    
+                    item.save()
+
+            # Handle the addition of a new item to a To-Do list
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("item content")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete = False) 
                 else:
-                    item.complete = False
-                
-                item.save()
+                    print("invalid input")
 
-        # Handle the addition of a new item to a To-Do list
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("item content")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete = False) 
-            else:
-                print("invalid input")
-
-    return render(response, "main/list.html", {"ls":ls})
+        return render(response, "main/list.html", {"ls":ls})
+    else:
+        return render(response, "main/view.html", {})
 
 def home(response):
     return render(response, "main/home.html", {})
@@ -41,11 +45,15 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data["name"]
-            t = ToDoList(name = n)
+            t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
         
         return HttpResponseRedirect("/%i" %t.id)
 
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
+
+def view(response):
+    return render(response, "main/view.html", {})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("user_registration/", v.user_registration, name="user_registration"),
     path('', include("main.urls")),
-    
+    path('', include("django.contrib.auth.urls"))
 ]
